b2a4e3f0c5e0bc74765464f585f0eaf4a7ac8ad0
Peeter Meos <peeterm@proekspert.ee>
2019-10-07 22:09:54 +0300

    Synthesis works

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
 from words import BeeWords
+import io
 
 app = Flask(__name__)
 my_bee = BeeWords("words.txt")
 
 
-@app.route('/random-word')
+@app.route('/random-word', methods=["GET"])
 def random_word():
-    return my_bee.get_word()
+    w = my_bee.get_word()
+    return w
+
+
+@app.route('/update', methods=["POST"])
+def update_stats():
+    if request.method == "POST":
+        pass
+        # print(dict(request.json))
+
+    w = my_bee.get_word()
+    return w
+
+
+@app.route('/polly')
+def polly():
+    w = request.args.get('w')
+    if w is not None:
+        a = my_bee.say_english(w)
+        return send_file(io.BytesIO(a),
+                         attachment_filename=w+'.mp3',
+                         mimetype='audio/mp3')
 
 
 @app.route('/')
 def index():
-    word = my_bee.get_word()
-    return render_template('index.html', word_est=word["estonian"], word_eng=word["english"])
+    w = my_bee.get_word()
+    a = my_bee.say_english(w["english"])
+    return render_template('index.html',
+                           word_est=w["estonian"], word_eng=w["english"], audio=a)
 
 
 if __name__ == '__main__':
--- words.py
+++ words.py
@@ -3,6 +3,7 @@ Dictionary object for spelling bee
 """
 import random
 import pandas as pd
+import boto3
 
 
 class BeeWords:
@@ -11,6 +12,15 @@ class BeeWords:
                       {'english': 'cat', 'estonian': 'kass'}]
         df = pd.read_csv(fn, delimiter=",")
         self.words = df.to_dict('records')
+        self.polly = boto3.Session(region_name="us-east-1").client("polly")
 
     def get_word(self):
         return random.choice(self.words)
+
+    def say_english(self, w):
+        response = self.polly.synthesize_speech(VoiceId='Joanna',
+                                                TextType="ssml",
+                                                OutputFormat="mp3",
+                                                Text='<prosody rate="65%">' +
+                                                     w + '</prosody>')
+        return response["AudioStream"].read()
